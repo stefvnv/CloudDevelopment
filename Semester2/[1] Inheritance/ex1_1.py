9f4cb546a6a308ec006ea0e8840ca652e1fdc37a
stefvnv <stefvnv@yahoo.com>
2022-01-17 23:27:46 +0000

    pushy

--- Semester2/[1] Inheritance/ex1_1.py
+++ Semester2/[1] Inheritance/ex1_1.py
@@ -1,13 +1,13 @@
-
-
 from tkinter import *
+
 window = Tk()
 window.geometry("300x300")
 window.title("Welcome")
 
-from Ex211MyClasses import *
+from ex1_1Class import *
 
-#------------end of class definition------------------
+
+# ------------end of class definition------------------
 
 def display():
     value1 = p1.getValue1()
@@ -17,9 +17,11 @@ def display():
     entry3.delete(0, END)  # delete old value
     entry3.insert(END, value2)
 
+
 def incrV1Event():
     p1.incrValue1()
     display()
+
 
 def incrV2Event():
     p1.incrValue2()
@@ -32,6 +34,7 @@ def addEvent():
     entry4.delete(0, END)  # delete old value
     entry4.insert(END, result)
 
+
 def multiplyEvent():
     result = p1.multiply()
     display()
@@ -41,24 +44,22 @@ def multiplyEvent():
 
 p1 = Pair(3, 5)
 
+label1 = Label(window, text="Welcome", fg="blue", bg="yellow", font=("arial", 16, "bold"))  #
+label1.place(x=90, y=30)  # place on screen
 
-label1 = Label(window, text="Welcome", fg="blue",bg="yellow", font=("arial", 16, "bold"))   #
-label1.place(x=90, y=30)                            # place on screen
-
-label2 = Label(window, text="Value1", fg="blue", width=8, font=("arial", 10, "bold"))   #
+label2 = Label(window, text="Value1", fg="blue", width=8, font=("arial", 10, "bold"))  #
 label2.place(x=10, y=80)
 
 entry2 = Entry(window)
 entry2.insert(END, '1')
 entry2.place(x=120, y=80)
 
-label3 = Label(window, text="Value2", fg="blue", width=8, font=("arial", 10, "bold"))   #
+label3 = Label(window, text="Value2", fg="blue", width=8, font=("arial", 10, "bold"))  #
 label3.place(x=10, y=110)
 
 entry3 = Entry(window)
 entry3.insert(END, '1')
 entry3.place(x=120, y=110)
-
 
 button1 = Button(window, text="IncrementV1", fg="black", font=("arial", 12, "bold"), command=incrV1Event)
 button1.place(x=10, y=140)
@@ -66,20 +67,19 @@ button1.place(x=10, y=140)
 button2 = Button(window, text="IncrementV2", fg="black", font=("arial", 12, "bold"), command=incrV2Event)
 button2.place(x=140, y=140)
 
-button4= Button(window, text="Add", fg="black", font=("arial", 10, "bold"), command=addEvent)
+button4 = Button(window, text="Add", fg="black", font=("arial", 10, "bold"), command=addEvent)
 button4.place(x=40, y=180)
 
 entry4 = Entry(window)
 entry4.insert(END, '')
 entry4.place(x=120, y=180)
 
-button5= Button(window, text="Multiply", fg="black", font=("arial", 10, "bold"), command=multiplyEvent)
+button5 = Button(window, text="Multiply", fg="black", font=("arial", 10, "bold"), command=multiplyEvent)
 button5.place(x=40, y=220)
 
 entry5 = Entry(window)
 entry5.insert(END, '')
 entry5.place(x=120, y=220)
-
 
 display()
 
